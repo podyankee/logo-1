6bfbeb6193c3fa9c8854f2c5c3ee74f29a49798d
podyankee <podyankee@gmail.com>
2020-08-02 17:18:16 +0300

    main slider added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-window.jQuery = $;
-window.$ = $;
-window.$ = window.jQuery = require('jquery');
+// window.jQuery = $;
+// window.$ = $;
+// window.$ = window.jQuery = require('jquery');
 
-// import 'slick-slider/slick/slick.css';
-// import 'fullpage.js/dist/fullpage.css';
-// import 'slick-slider/slick/slick';
-// import 'fullpage.js/dist/fullpage';
+
+
 
 import './assets/scripts/common';
 
 // Styles
 import 'normalize.css';
+import 'swiper/swiper-bundle.css';
 import "styles/_index.sass";
 
 window.onload = function () {
